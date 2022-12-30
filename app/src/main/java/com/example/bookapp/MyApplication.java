ff8234c7a81ce2a33035b964d8ee789619b30081
kavindukavishwara <kwanigarachchi@gmail.com>
2022-12-30 20:26:55 +0530

    Add comments

--- app/src/main/java/com/example/bookapp/MyApplication.java
+++ app/src/main/java/com/example/bookapp/MyApplication.java
@@ -3,12 +3,17 @@ package com.example.bookapp;
 import static com.example.bookapp.Constants.MAX_BYTES_PDF;
 
 import android.app.Application;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.Environment;
 import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.LongDef;
 import androidx.annotation.NonNull;
 
 import com.example.bookapp.adapters.AdapterPdfAdmin;
@@ -28,11 +33,15 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageMetadata;
 import com.google.firebase.storage.StorageReference;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Locale;
 
 public class MyApplication extends Application {
+
+    private  static  final  String TAG_DOWNLOAD = "DOWNLOAD_TAG";
 
     @Override
     public  void onCreate(){
@@ -182,5 +191,113 @@ public class MyApplication extends Application {
 
                     }
                 });
+    }
+
+    public static void downloadBook(Context context,String bookId, String bookTitle ,String bookUrl){
+        Log.d(TAG_DOWNLOAD, "downloadBook:downloading book...");
+
+        String nameWithExtension= bookTitle + ".pdf";
+        Log.d(TAG_DOWNLOAD, "downloadBook:NAME: "+nameWithExtension);
+
+        ProgressDialog progressDialog = new ProgressDialog(context);
+        progressDialog.setTitle("Please wait");
+        progressDialog.setMessage("Downloading"+ nameWithExtension+"...");
+        progressDialog.setCanceledOnTouchOutside(false);
+        progressDialog.show();
+
+        StorageReference storageReference = FirebaseStorage.getInstance().getReferenceFromUrl(bookUrl);
+        storageReference.getBytes(MAX_BYTES_PDF)
+                .addOnSuccessListener(new OnSuccessListener<byte[]>() {
+                    @Override
+                    public void onSuccess(byte[] bytes) {
+                        Log.d(TAG_DOWNLOAD, "onSucess:Book Downloaded");
+                        saveDownloadedBook(context,progressDialog,bytes,nameWithExtension,bookId);
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(TAG_DOWNLOAD, "onFailure:Failed to download due to"+e.getMessage());
+                        progressDialog.dismiss();
+                        Toast.makeText(context,"Failed to download due to"+e.getMessage(),Toast.LENGTH_SHORT).show();
+
+
+                    }
+                });
+
+    }
+
+    private static void saveDownloadedBook(Context context, ProgressDialog progressDialog, byte[] bytes, String nameWithExtension, String bookId) {
+
+        Log.d(TAG_DOWNLOAD, "onSuccess: Saving book");
+
+        try {
+            File downloadsFolder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+            downloadsFolder.mkdirs();
+
+            String filePath = downloadsFolder.getPath() +"/"+nameWithExtension;
+
+            FileOutputStream out =new FileOutputStream(filePath);
+            out.write(bytes);
+            out.close();
+
+            Toast.makeText(context, "Saved to Download Folder", Toast.LENGTH_SHORT).show();
+            progressDialog.dismiss();
+            incrementBookDownloadCount(bookId);
+
+        }
+        catch (Exception e){
+            Log.d(TAG_DOWNLOAD, "saveDownloadedBook: Failed saving to Download Folder due to "+e.getMessage());
+            Toast.makeText(context,"Failed saving to Download Folder due to" +e.getMessage(),Toast.LENGTH_SHORT).show();
+            progressDialog.dismiss();
+        }
+    }
+
+    private static void incrementBookDownloadCount(String bookId) {
+        Log.d(TAG_DOWNLOAD, "incrementBookDownloadCount: Incrementing Book Download Count");
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Books");
+        ref.child(bookId)
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        String  downloadsCount = ""+snapshot.child("downloadsCount").getValue();
+                        Log.d(TAG_DOWNLOAD, "onDataChange: Downloads Count:"+downloadsCount);
+
+                        if (downloadsCount.equals("")|| downloadsCount.equals("null")){
+                            downloadsCount="0";
+
+                        }
+                        long newDownloadsCount = Long.parseLong(downloadsCount)+ 1;
+                        Log.d(TAG_DOWNLOAD, "onDataChange: New Download Count:"+newDownloadsCount);
+
+                        HashMap<String,Object>hashMap =new HashMap<>();
+                        hashMap.put("downloadsCount",newDownloadsCount);
+
+                        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Books");
+                        reference.child(bookId).updateChildren(hashMap)
+                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void unused) {
+                                        Log.d(TAG_DOWNLOAD, "onSuccess: Downloads Count update...");
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Log.d(TAG_DOWNLOAD, "onFailure: Failed to update downloads Count due to"+e.getMessage());
+                                    }
+                                });
+
+                        ;
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
     }
 }
--- app/src/main/java/com/example/bookapp/PdfDetailActivity.java
+++ app/src/main/java/com/example/bookapp/PdfDetailActivity.java
@@ -1,21 +1,35 @@
 package com.example.bookapp;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.example.bookapp.MyApplication;
+import com.example.bookapp.adapters.AdapterComment;
+import com.example.bookapp.models.ModelComment;
 import com.github.barteksc.pdfviewer.PDFView;
 import com.example.bookapp.databinding.ActivityPdfDetailBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 
@@ -25,7 +39,14 @@ public class PdfDetailActivity extends AppCompatActivity {
     private ActivityPdfDetailBinding binding;
 
     //pdf id get from intent
-    String bookId;
+    String bookId,bookTitle,bookUrl;
+    private  static  final  String TAG_DOWNLOAD = "DOWNLOAD_TAG";
+
+    private ProgressDialog progressDialog;
+
+    private ArrayList<ModelComment>commentArrayList;
+    private AdapterComment adapterComment;
+
 
 
     @Override
@@ -37,6 +58,12 @@ public class PdfDetailActivity extends AppCompatActivity {
         // get data from intent book id
         Intent intent = getIntent();
         bookId = intent.getStringExtra("bookId");
+
+        binding.downloadBookBtn.setVisibility(View.GONE);
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Please wait");
+        progressDialog.setCanceledOnTouchOutside(false);
 
         loadBookDetails();
         //increment view count
@@ -60,7 +87,99 @@ public class PdfDetailActivity extends AppCompatActivity {
             }
         });
 
+        binding.downloadBookBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG_DOWNLOAD, "onClick: Cheking permission");
+                if (ContextCompat.checkSelfPermission(PdfDetailActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)== PackageManager.PERMISSION_GRANTED){
+                    Log.d(TAG_DOWNLOAD, "onClick: Permission already granted, can download book");
+                    MyApplication.downloadBook(PdfDetailActivity.this,""+bookId,""+bookTitle,""+bookUrl);
+                }
+                else {
+                    Log.d(TAG_DOWNLOAD, "onClick: Permission was not granted, request permission..");
+                    requestPermissionLauncher.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+                }
+
+
+            }
+        });
+
     }
+
+    private void loadComment(){
+        commentArrayList =new ArrayList<>();
+
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Books");
+        ref.child(bookId).child("Comments")
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        comment.clear();
+                        for(DataSnapshot ds:snapshot.getChildren()){
+                            ModelComment model = ds.getValue(ModelComment.class);
+                            commentArrayList.add(model);
+                        }
+                        adapterComment = new AdapterComment(PdfDetailActivity.this,commentArrayList);
+                        binding.commentsRv.setAdapter(adapterComment);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                })
+    }
+    private String comment="";
+
+    private void addComment(){
+        progressDialog.setMessage("Adding comment...");
+        progressDialog.show();
+
+        String timestamp =""+System.currentTimeMillis();
+
+        HashMap<String ,Object> hashMap= new HashMap<>();
+        hashMap.put("id",""+timestamp);
+        hashMap.put("bookId",""+bookId);
+        hashMap.put("comment",""+comment);
+        hashMap.put("uid",""+firebaseAuth.getUid());
+
+        DatabaseReference ref =FirebaseDatabase.getInstance().getReference("Books");
+        ref.child(bookId).child("Comment").child(timestamp)
+                .setValue(hashMap)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+
+                        Toast.makeText(PdfDetailActivity.this,"Comment Added...",Toast.LENGTH_SHORT).show();
+                        progressDialog.dismiss();
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                        progressDialog.dismiss();
+                        Toast.makeText(PdfDetailActivity.this,"Failed to add comment due to "+e.getMessage(),Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+
+
+    }
+
+    private ActivityResultLauncher<String> requestPermissionLauncher =
+            registerForActivityResult(new ActivityResultContracts.RequestPermission(),isGranted->{
+                if(isGranted){
+                    Log.d(TAG_DOWNLOAD, "Permission Granted ");
+                    MyApplication.downloadBook(this,""+bookId,""+bookTitle,""+bookUrl);
+                }
+                else {
+                    Log.d(TAG_DOWNLOAD, ": Permission was denied");
+                    Toast.makeText(this, "Permission was denied", Toast.LENGTH_SHORT).show();
+                }
+
+            });
 
     private void loadBookDetails() {
         DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Books");
@@ -69,13 +188,15 @@ public class PdfDetailActivity extends AppCompatActivity {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot snapshot) {
                         //get data
-                        String title = ""+snapshot.child("title").getValue();
+                        bookTitle = ""+snapshot.child("title").getValue();
                         String description = ""+snapshot.child("description").getValue();
                         String categoryId = ""+snapshot.child("categoryId").getValue();
                         String viewsCount = ""+snapshot.child("viewsCount").getValue();
                         String downloadsCount = ""+snapshot.child("downloadsCount").getValue();
-                        String url = ""+snapshot.child("url").getValue();
+                        bookUrl = ""+snapshot.child("url").getValue();
                         String timestamp = ""+snapshot.child("timestamp").getValue();
+
+                        binding.downloadBookBtn.setVisibility(View.VISIBLE);
 
                         //format data
                         String date = MyApplication.formatTimestamp(Long.parseLong(timestamp));
@@ -84,22 +205,22 @@ public class PdfDetailActivity extends AppCompatActivity {
                                 binding.categoryTv
                         );
                         MyApplication.loadPdfFromUrlSinglePage(
-                                ""+url,
-                                ""+title,
+                                ""+bookUrl,
+                                ""+bookTitle,
                                 binding.pdfView,
                                 binding.progressBar
                         );
                         MyApplication.loadPdfSize(
-                                ""+url,
-                                ""+title,
+                                ""+bookUrl,
+                                ""+bookTitle,
                                 binding.sizeTv
                         );
                         //set data
-                                binding.titleTv.setText(title);
-                                binding.descriptionTv.setText(description);
-                                binding.viewsTv.setText(viewsCount.replace("null","N/A"));
-                                binding.downloadsTv.setText(downloadsCount.replace("null","N/A"));
-                                binding.dateTv.setText(date);
+                        binding.titleTv.setText(bookTitle);
+                        binding.descriptionTv.setText(description);
+                        binding.viewsTv.setText(viewsCount.replace("null","N/A"));
+                        binding.downloadsTv.setText(downloadsCount.replace("null","N/A"));
+                        binding.dateTv.setText(date);
 
                     }
 
